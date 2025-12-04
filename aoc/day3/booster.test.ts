1a865a66d8ff1636657c913185ade2f104414b3b
Khang Woei <41323253+KhangWoei@users.noreply.github.com>
2025-12-04 14:06:01 +0000

    day3 part 2

--- aoc/day3/booster.test.ts
+++ aoc/day3/booster.test.ts
@@ -8,11 +8,24 @@ describe("booster", () => {
         { bank: "811111111111119", expected: 89 },
         { bank: "234234234234278", expected: 78 },
         { bank: "818181911112111", expected: 92 },
-        { bank: "1212422224352324224333264212322156522232312432522422111222222521313812221112222142322344111253222322", expected: 85 },
     ])("getLargestJoltage, given $bank, largest joltage should be $expected", ({ bank, expected }) => {
         const booster = new Booster();
 
         const actual = booster.getLargestJoltage(bank);
+
+        expect(actual).toBe(expected);
+    });
+
+
+    test.each([
+        { bank: "987654321111111", expected: 987654321111 },
+        { bank: "811111111111119", expected: 811111111119 },
+        { bank: "234234234234278", expected: 434234234278 },
+        { bank: "818181911112111", expected: 888911112111 },
+    ])("getLargestJoltage2, given $bank, largest joltage should be $expected", ({ bank, expected }) => {
+        const booster = new Booster();
+
+        const actual = booster.getLargestJoltage2(bank);
 
         expect(actual).toBe(expected);
     });
--- aoc/day3/booster.ts
+++ aoc/day3/booster.ts
@@ -1,5 +1,6 @@
 export type Result = {
     part1: number
+    part2: number
 }
 
 export class Booster {
@@ -7,13 +8,15 @@ export class Booster {
 
     constructor() {
         this._result = {
-            part1: 0
+            part1: 0,
+            part2: 0
         }
     }
 
     public async boostAsync(banks: AsyncIterable<string>): Promise<Result> {
         for await (const bank of banks) {
             this._result.part1 += this.getLargestJoltage(bank);
+            this._result.part2 += this.getLargestJoltage2(bank);
         }
         return this._result;
     }
@@ -36,5 +39,31 @@ export class Booster {
         }
 
         return maxJoltage;
+    }
+
+    public getLargestJoltage2(bank: string): number {
+        let result = 0;
+        let start = bank.length - 12;
+        let end = 0;
+
+        while (start >= end && start < bank.length) {
+            let maxIndex = start;
+
+            if (start !== end) {
+
+                for (let current = start - 1; current >= end; current--) {
+                    if (parseInt(bank[maxIndex]) <= parseInt(bank[current])) {
+                        maxIndex = current;
+                    }
+                }
+
+            }
+
+            result = (result * 10) + parseInt(bank[maxIndex]);
+            end = maxIndex + 1;
+            start++;
+        }
+
+        return result;
     }
 }
