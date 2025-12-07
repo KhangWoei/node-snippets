56ade44304f34cbfadd4f951458354d59aa202f3
Khang <41323253+KhangWoei@users.noreply.github.com>
2025-12-07 00:10:00 +0000

    day 4 part 1
    should have trusted my tests spend hours debugging it wondering why it
    was wrong just to realise that my input file was wrong...

--- aoc/day4/sweeper.test.ts
+++ aoc/day4/sweeper.test.ts
@@ -1,13 +1,82 @@
-import { describe, test, expect } from "vitest"
-import { Sweeper } from "./sweeper.js";
+import { describe, test, expect } from 'vitest'
+import { Sweeper } from './sweeper.js';
 
-describe("Sweeper", () => {
-    test("sweep should return number of accessible rolls", () => {
+describe('Sweeper', () => {
+    test.each([
+        {
+            input: [
+                ".",
+            ],
+            expected: 0
+        },
+        {
+            input: [
+                "@"
+            ],
+            expected: 1
+        },
+        {
+            input: [
+                "@@"
+            ],
+            expected: 2
+        },
+        {
+            input: [
+                "@@@"
+            ],
+            expected: 3
+        },
+        {
+            input: [
+                "@",
+                "@",
+            ],
+            expected: 2
+        },
+        {
+            input: [
+                "@@",
+                "@@"
+            ],
+            expected: 4
+        },
+        {
+            input: [
+                "@@@",
+                "@@@"
+            ],
+            expected: 4
+        },
+        {
+            input: [
+                "@",
+                "@",
+                "@"
+            ],
+            expected: 3
+        },
+        {
+            input: [
+                "@@",
+                "@@",
+                "@@"
+            ],
+            expected: 4
+        },
+        {
+            input: [
+                "@@@",
+                "@@@",
+                "@@@"
+            ],
+            expected: 4
+        },
+    ])("sweep, return number of rolls with less than 4 rolls: $input, $expected", ({ input, expected }) => {
         const sweeper = new Sweeper();
-        const grid = new Array<string>();
 
-        const actual = sweeper.sweep(grid);
+        const actual = sweeper.sweep(input);
 
-        expect(actual).toBe(0);
+        expect(actual.part1).toBe(expected);
     });
 });
--- aoc/day4/sweeper.ts
+++ aoc/day4/sweeper.ts
@@ -1,3 +1,5 @@
+import { argv0 } from "process";
+
 export type Result = {
     part1: number
 }
@@ -12,6 +14,31 @@ export class Sweeper {
     }
 
     public sweep(grid: Array<string>): Result {
+
+        grid.forEach((row, rowIndex) => {
+            for (let colIndex = 0; colIndex < row.length; colIndex++) {
+                if (row[colIndex] === "@"
+                    && this.isAccessible(rowIndex, colIndex, grid)) {
+                    this._result.part1++;
+                }
+            }
+        });
+
         return this._result;
+    }
+    private isAccessible(rowIndex: number, colIndex: number, grid: Array<string>): boolean {
+        return (isRoll(rowIndex - 1, colIndex - 1) + isRoll(rowIndex - 1, colIndex) + isRoll(rowIndex - 1, colIndex + 1)
+            + isRoll(rowIndex, colIndex - 1) + isRoll(rowIndex, colIndex + 1)
+            + isRoll(rowIndex + 1, colIndex - 1) + isRoll(rowIndex + 1, colIndex) + isRoll(rowIndex + 1, colIndex + 1)) < 4
+
+        function isRoll(row: number, column: number): number {
+            return row < 0
+                || row >= grid.length
+                || column < 0
+                || column >= grid[0].length
+                || grid[row][column] !== "@"
+                ? 0
+                : 1
+        }
     }
 }
